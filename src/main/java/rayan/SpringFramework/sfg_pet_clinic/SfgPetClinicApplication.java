37c0fab22d3d915431af4e69eed9dafe2ec4841c
reobest <sabdelrahman071@gmail.com>
2025-10-22 21:53:34 +0200

    first commit

---
+++ src/main/java/rayan/SpringFramework/sfg_pet_clinic/SfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package rayan.SpringFramework.sfg_pet_clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgPetClinicApplication.class, args);
+	}
+
+}
