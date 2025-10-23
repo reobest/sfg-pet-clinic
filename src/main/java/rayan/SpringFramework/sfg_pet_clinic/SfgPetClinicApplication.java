72aab39cfff7abe37e73cb476760a4295377ddf4
reobest <sabdelrahman071@gmail.com>
2025-10-24 00:16:08 +0200

    colsing the issue number 2

--- src/main/java/rayan/SpringFramework/sfg_pet_clinic/SfgPetClinicApplication.java
+++ src/main/java/rayan/SpringFramework/sfg_pet_clinic/SfgPetClinicApplication.java
@@ -2,12 +2,21 @@ package rayan.SpringFramework.sfg_pet_clinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import rayan.SpringFramework.sfg_pet_clinic.controllers.MyController;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
+
 	public static void main(String[] args) {
-		SpringApplication.run(SfgPetClinicApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgPetClinicApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String result = myController.sayHello();
+		System.out.println(result);
 	}
 
 }
