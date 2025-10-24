771bdd6c6c85eb38e7dff150081dc78883eeffee
reobest <sabdelrahman071@gmail.com>
2025-10-24 22:53:10 +0200

    added modules

---
+++ pet-clinic-web/src/test/java/rayan/SpringFramework/sfg_pet_clinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rayan.SpringFramework.sfg_pet_clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
